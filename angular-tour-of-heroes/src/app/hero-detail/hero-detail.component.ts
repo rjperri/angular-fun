d4eecb373d8f203a1e8ad2a3b7ed93963eaa1504
Robert Perri <bob.perri@figmarketing.com>
2017-12-22 15:06:27 -0500

    added http requests add,search,put,delete,get,get all

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute }           from '@angular/router';
-import { Location }                 from '@angular/common';
+import { Component, OnInit, Input }     from '@angular/core';
+import { ActivatedRoute }               from '@angular/router';
+import { Location }                     from '@angular/common';
 
 import { Hero }         from '../hero';
 import { HeroService }  from '../hero.service';
@@ -33,6 +33,12 @@ export class HeroDetailComponent implements OnInit {
 
     goBack(): void {
         this.location.back();
-      }
+    }
+
+    save(): void {
+        this.heroService
+            .updateHero(this.hero)
+            .subscribe(() => this.goBack());
+    }
 
 }
---
+++ angular-tour-of-heroes/src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit }    from '@angular/core';
+
+import { Hero }         from '../hero';
+import { HeroService }  from '../hero.service';
+
+import { Observable }   from 'rxjs/Observable';
+import { Subject }      from 'rxjs/Subject';
+import { of }           from 'rxjs/observable/of';
+import {
+    debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+
+
+
+
+@Component({
+    selector: 'app-hero-search',
+    templateUrl: './hero-search.component.html',
+    styleUrls: ['./hero-search.component.css']
+})
+export class HeroSearchComponent implements OnInit {
+
+    heroes$: Observable<Hero[]>;
+    private searchTerms = new Subject<string>();
+
+    constructor(private herorService: HeroService) { }
+
+    ngOnInit() {
+        this.heroes$ = this.searchTerms.pipe(
+            // wait 300ms after each keystroke before considering the term
+            debounceTime(300),
+
+            // ignore new term if same as previos term
+            distinctUntilChanged(),
+
+            // switch to new search observable each time the term changes
+            switchMap((term: string) => this.herorService.searchHeroes(term))
+        )
+    }
+
+    // Push a search term into the observable stream.
+    search(term: string): void {
+        this.searchTerms.next(term);
+    }
+
+}
