bcf3aaa61561e739c30446d0b976b6fb64583ec3
Robert Perri <bob.perri@figmarketing.com>
2017-12-22 11:46:07 -0500

    fixed imports

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
-import { MessageService } from './message.service';
+import { Injectable }   from '@angular/core';
+
+import { Hero }             from './hero';
+import { HEROES }           from './mock-heroes';
+import { MessageService }   from './message.service';
+
+import { Observable }   from 'rxjs/Observable';
+import { of }           from 'rxjs/observable/of';
 
 @Injectable()
 export class HeroService {
